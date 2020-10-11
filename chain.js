a94a472a238d78b3b971be4a9653f9d637a9f4ff
abbas579 <drdeebahasan@gmail.com>
2020-10-11 11:42:09 +0530

    constraint

---
+++ chain.js
@@ -0,0 +1,18 @@
+class Chain{
+constructor(bodyA,bodyB){
+    var options={
+bodyA:bodyA,
+bodyB:bodyB,
+length:200,
+stiffness:0.03
+    }
+  this.chain= Matter.Constraint.create(options);
+  World.add(world,this.chain)
+}
+display(){
+var pointA=this.chain.bodyA.position
+var pointB=this.chain.bodyB.position
+strokeWeight(2);
+line(pointA.x,pointA.y,pointB.x,pointB.y)
+}
+}
